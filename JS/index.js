61bd2777f6a985370306a577f527a167f8446472
Chukwu Gabriel <aquillaorie@gmail.com>
2020-04-18 21:48:15 +0100

    Responsive Navigation Bar
    Created a simple responsive nav bar using HTML, CSS and JS.

--- JS/index.js
+++ JS/index.js
@@ -1,2 +1,21 @@
-var hello = "Hello World 'I am a Javascript programmer'";
-document.write(hello);
+const navbarToggler = document.querySelector(".navbar-toggler");
+const navbarMenu = document.querySelector(".navbar ul");
+const navbarLinks = document.querySelectorAll(".navbar a");
+
+navbarToggler.addEventListener("click", navbarTogglerClick);
+
+function navbarTogglerClick()
+{
+	navbarToggler.classList.toggle("open-navbar-toggle");
+	navbarMenu.classList.toggle("open");
+}
+
+navbarLinks.forEach(elem => elem.addEventListener("click", navbarLinkClick));
+
+function navbarLinkClick()
+{
+	if (navbarMenu.classList.contains("open")) 
+	{
+		navbarToggler.click();
+	}
+}
